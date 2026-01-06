95f7ce8f19533551556596022cb20fac96961e39
tevoshw <tevoshw@gmail.com>
2026-01-06 14:42:51 -0300

    add CONTROL STRUCTURE: SWITCH-CASE in JAVA

---
+++ control_structures/switch_case.java
@@ -0,0 +1,24 @@
+package control_structures;
+
+public class switch_case {
+    public static void main(String[] args) {
+        char x = 'A';
+
+    switch (x)
+    {
+    case 'A':
+        System.out.println("A");
+        break;
+
+    case 'B':
+        System.out.println("B");
+        break;
+
+    default:
+        System.out.println("Other than A and B");
+        break;
+    }
+    
+    }
+    
+}
